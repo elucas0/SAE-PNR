5116f57a35bc463199c54dcc44e9ddbe6d7d4c78
Timothee <timothee.posselt@gmail.com>
2022-05-18 12:34:03 +0200

    m

--- SAE_PNR/src/Scenario.java
+++ SAE_PNR/src/Scenario.java
@@ -37,7 +37,7 @@ public class Scenario {
         ArrayList<Observateur> observateurs = new ArrayList<Observateur>();
         int[] obs = new int[1];
         Lieu lieu = new Lieu(785, 154);
-        ObsLoutre loutre = new ObsLoutre(1, new Date(1), new Time(1) , lieu, observateurs, obs,  IndiceLoutre.POSITIF);
+        ObsLoutre loutre = new ObsLoutre(1, new Date(1), new Time(1) , lieu, observateurs,IndiceLoutre.POSITIF);
         System.out.println(loutre.getIndice());
         loutre.setIndice(IndiceLoutre.NEGATIF);
     }
