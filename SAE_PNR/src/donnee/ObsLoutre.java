86d0bcdafc3e85d116aef9d34aba8c0a87cb87d0
Timothee <timothee.posselt@gmail.com>
2022-05-18 12:26:26 +0200

    m

--- SAE_PNR/src/donnee/ObsLoutre.java
+++ SAE_PNR/src/donnee/ObsLoutre.java
@@ -1,43 +1,47 @@
 package donnee;
-import java.util.ArrayList;
-import java.sql.Time;
 import java.sql.Date;
-
-/**
- * 
- */
+import java.sql.Time;
+import java.util.*;
+/**Classe pour cree les observations des loutres */
 public class ObsLoutre extends Observation {
-
+	/**Parametre pour les indices des loutres */
 	private IndiceLoutre indice;
 
 	/**
-	 * Create the observed otter
-	 * @param id id of the observation
-	 * @param date date of the observation
-	 * @param heure time of the observation
-	 * @param lieu location of the observation
-	 * @param observateur list of the observers
-	 * @param indice the state of the place(not visited, with otter, or without otter)
+	 * Constructeur pour crée les observations des batraciens
+	 * @param id l'id de observation
+	 * @param date date de l'observation
+	 * @param heure heure de l'observation
+	 * @param lieu lieu de l'observation
+	 * @param observateurs nom des observateurs
 	 */
-	public ObsLoutre(int id, Date date, Time heure, Lieu lieu, ArrayList<Observateur> observateur, IndiceLoutre indice) {
-		super(id, date, heure, lieu, observateur);
-
+	public ObsLoutre(int id, Date date, Time heure, Lieu lieu, ArrayList<Observateur> observateurs, IndiceLoutre indice) {
+		super(id, date, heure, lieu, observateurs);
 		if(indice != null){
-
 			this.indice = indice;
-		}else{
-
-			throw new UnsupportedOperationException("ObsLoutre : indice must not be null");
 		}
-
+		else{
+			System.err.println("ObsLoutre : paramètre érroné");
+		}
+		throw new UnsupportedOperationException();
 	}
-
 	/**
-	 * Get the specie
+	 * Methode pour avoir l'indice
+	 * @return l'inice
 	 */
-	public EspeceObservee especeObs() {
-		return EspeceObservee.LOUTRE;
+	public IndiceLoutre getIndice() {
+		return indice;
 	}
-
-
+	/**
+	 * Methode pour changer l'indice
+	 * @param indice le nouvel indice
+	 */
+	public void setIndice(IndiceLoutre indice) {
+		if(indice != null){
+			this.indice = indice;
+		}
+		else{
+			System.err.println("setIndice : paramètre érroné");
+		}
+	}
 }
