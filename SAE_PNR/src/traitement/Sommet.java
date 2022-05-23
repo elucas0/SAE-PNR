37caae5998ccf22bca6d783e32f80aa552bfbfe2
Timothee <timothee.posselt@gmail.com>
2022-05-23 09:28:00 +0200

    sommet

--- SAE_PNR/src/traitement/Sommet.java
+++ SAE_PNR/src/traitement/Sommet.java
@@ -1,5 +1,21 @@
+import donnee.*;
 package traitement;
 
 public class Sommet {
-    
+    private int id;
+    private Lieu coordLieu;
+    private Date date;
+    private EspeceObservee espece;
+
+    public Sommet(int id, Lieu coord, Date date, EspeceBatracien espece){
+
+    }
+
+    public Sommet(Observation obs){
+
+    }
+
+    public double calculeDist(Sommet som){
+
+    }
 }
