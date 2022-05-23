7ad3ea8209ffbf9fa20238d2a12185b804c3b862
Lemap1 <maelphilippe25@gmail.com>
2022-05-23 08:28:12 +0200

    lo

--- SAE_PNR/src/traitement/Graphe.java
+++ SAE_PNR/src/traitement/Graphe.java
@@ -6,4 +6,25 @@ public class Graphe {
 
     private HashMap<Sommet, ArrayList<Sommet>> sommetsVoisins;
     
+
+    public HashMap<Sommet, Integer> calculDegre(){
+
+        HashMap<Sommet, Integer> ret = null;
+
+        if(this.sommetsVoisins.size() > 0){
+
+            for(Sommet i : this.sommetsVoisins.keySet()){
+
+                ret.put(i, this.sommetsVoisins.get(i).size());
+            }
+
+        }else{
+
+            System.err.println("calculDegre : there must be at least one value in sommetsVoisins.");
+        }
+
+
+
+        return ret;
+    }
 }
