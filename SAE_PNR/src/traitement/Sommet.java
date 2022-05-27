b61f7be7120505737f7a439d88ab1b4a65736b2e
Elouann Lucas <elouannlucas56@gmail.com>
2022-05-27 12:35:01 +0200

    Ajout commentaires et aération de la classe Sommets

--- SAE_PNR/src/traitement/Sommet.java
+++ SAE_PNR/src/traitement/Sommet.java
@@ -9,37 +9,47 @@ public class Sommet {
     private Date date;
     private EspeceObservee espece;
 
+    /**
+     * Constructor of the class Sommet
+     * @param id the id of the vertex
+     * @param coord the coordinates of the vertex
+     * @param date the date of the observation
+     * @param espece the species of the observation
+     */
     public Sommet(int id, Lieu coord, Date date, EspeceObservee espece){
+        //The id must be over 0
         if(id >= 1){
             this.id = id;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Sommet : Erreur dans l'id");
         }
         
+        //The coordLieu must be not null
         if(coord != null){
             this.coordLieu = coord;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Sommet : coord should not be null");
         }
 
+        //The date must be not null
         if(date != null){
             this.date = date;
-        }
-        else{
+        } else{
             throw new IllegalArgumentException("Sommet : date should not be null");
         }
 
+        //The espece must be not null
         if(espece != null){
             this.espece = espece;
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Sommet : espece should not be null");
         }
-
     }
 
+    /**
+     * Constructor of the class Sommet with an Observation
+     * @param obs the Observation
+     */
     public Sommet(Observation obs){
         if(obs != null){
             new Sommet(obs.getId(), obs.getLieu(), obs.getDate(), espece);
@@ -50,6 +60,11 @@ public class Sommet {
         
     }
 
+    /**
+     * Calculate the distance between two vertices
+     * @param som the second vertex
+     * @return the distance between the two vertices
+     */
     public double calculeDist(Sommet som){
         final int R = 6371; // Radious of the earth
 
@@ -72,36 +87,92 @@ public class Sommet {
         return resultat;
     }
 
+    /**
+     * Convert a degree to radian
+     * @param value
+     * @return
+     */
     private static Double toRad(Double value) {
         return value * Math.PI / 180;
     }
 
-
-
-
+    /**
+     * Get the date of the observation
+     * @return the date of the observation
+     */
     public Date getDate() {
         return date;
     }
+
+    /**
+     * Get the id of the vertex
+     * @return the id of the vertex
+     */
     public int getId(){
         return this.id;
     }
+
+    /**
+     * Get the coordinates of the vertex, the place where the observation was made
+     * @return the coordinates of the vertex
+     */
     public Lieu getCoordLieu() {
         return coordLieu;
     }
+
+    /**
+     * Get the species of the observation
+     * @return the species of the observation
+     */
     public EspeceObservee getEspece() {
         return espece;
     }
 
+    /**
+     * Set the coordinates of the vertex, the place where the observation was made
+     * @param coordLieu the coordinates of the vertex
+     */
     public void setCoordLieu(Lieu coordLieu) {
-        this.coordLieu = coordLieu;
+        if (coordLieu != null) {
+            this.coordLieu = coordLieu;
+        } else {
+            throw new IllegalArgumentException("Sommet : coordLieu should not be null");
+        }
     }
+    
+    /**
+     * Set the date of the observation
+     * @param date the date of the observation
+     */
     public void setDate(Date date) {
-        this.date = date;
+        if (date != null) {
+            this.date = date;
+        } else {
+            throw new IllegalArgumentException("Sommet : date should not be null");
+        }
     }
+
+    /**
+     * Set the species of the observation
+     * @param espece the species of the observation
+     */
     public void setEspece(EspeceObservee espece) {
-        this.espece = espece;
+        if (espece != null) {
+            this.espece = espece;
+        } else {
+            throw new IllegalArgumentException("Sommet : espece should not be null");
+        }
     }
+
+    /**
+     * Set the id of the vertex
+     * @param id the id of the vertex
+     */
     public void setId(int id) {
-        this.id = id;
+        if (id >= 1) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("Sommet : id should be over 0");
+        }
     }
 }
