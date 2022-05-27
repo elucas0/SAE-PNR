f1bf19e32114a6819e24249b3760d9dba1008f4b
Timothee <timothee.posselt@gmail.com>
2022-05-27 19:53:27 +0200

    javadoc sommet

--- SAE_PNR/src/traitement/Sommet.java
+++ SAE_PNR/src/traitement/Sommet.java
@@ -2,13 +2,25 @@ package traitement;
 import java.sql.Date;
 import donnee.*;
 
-
+/**
+ * Create one top for an observation
+ */
 public class Sommet {
+    /**id of the top */
     private int id;
+    /**coordinates of the top */
     private Lieu coordLieu;
+    /**Date of the top */
     private Date date;
+    /**species of the top */
     private EspeceObservee espece;
-
+    /**
+     * Generate a top
+     * @param id id of the top
+     * @param coord coordinates of the top
+     * @param date date of the top
+     * @param espece species of the top
+     */
     public Sommet(int id, Lieu coord, Date date, EspeceObservee espece){
         if(id >= 1){
             this.id = id;
@@ -40,6 +52,10 @@ public class Sommet {
 
     }
 
+    /**
+     * generate a top with an observation
+     * @param obs observation wwith wich create the top
+     */
     public Sommet(Observation obs){
         if(obs != null){
             new Sommet(obs.getId(), obs.getLieu(), obs.getDate(), espece);
@@ -50,6 +66,11 @@ public class Sommet {
         
     }
 
+    /**
+     * Method to calculate the distance between two tops 
+     * @param som the top with wich calculate the distance
+     * @return the distance between the two tops
+     */
     public double calculeDist(Sommet som){
         final int R = 6371; // Radious of the earth
 
@@ -72,35 +93,71 @@ public class Sommet {
         return resultat;
     }
 
+    /**
+     * Method to calculate the distance
+     * @param value double value with wich the method calculate the distance
+     * @return the distance
+     */
     private static Double toRad(Double value) {
         return value * Math.PI / 180;
     }
 
 
 
-
+    /**
+     * Return the date of the top
+	 * @return date of the top
+     */
     public Date getDate() {
         return date;
     }
+    /**
+     * Return the id of the top
+	 * @return id of the top
+     */
     public int getId(){
         return this.id;
     }
+    /**
+     * Return the coordinates of the top
+     * @return coordinates of the top
+     */
     public Lieu getCoordLieu() {
         return coordLieu;
     }
+    /**
+     * Return the species of the top
+     * @return species of the top
+     */
     public EspeceObservee getEspece() {
         return espece;
     }
 
+    /**
+     * Set the coordinates of the top
+     * @param coordLieu coordinates of the top
+     */
     public void setCoordLieu(Lieu coordLieu) {
         this.coordLieu = coordLieu;
     }
+    /**
+     * Set the date of the top 	
+	 * @param date date of the top
+	 */
     public void setDate(Date date) {
         this.date = date;
     }
+    /**
+     * Set the species for the top
+     * @param espece species of the top
+     */
     public void setEspece(EspeceObservee espece) {
         this.espece = espece;
     }
+    /**
+     * Set the id of the top
+	 *  @param id id of the top
+	 */
     public void setId(int id) {
         this.id = id;
     }
