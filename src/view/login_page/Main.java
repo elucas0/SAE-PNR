27ccef1eccbe31751cfce30773b8a0f679a65794
Elouann Lucas <elouannlucas56@gmail.com>
2022-06-02 11:31:48 +0200

    chepa

---
+++ src/view/login_page/Main.java
@@ -0,0 +1,22 @@
+package view.login_page;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        primaryStage.setTitle("PNR - Login");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
