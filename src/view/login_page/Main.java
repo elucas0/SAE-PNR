66cd888a094ff3272475fb6bdd6576c57fa53397
Elouann Lucas <elouannlucas56@gmail.com>
2022-06-02 12:34:46 +0200

    ssus

--- src/view/login_page/Main.java
+++ src/view/login_page/Main.java
@@ -2,17 +2,21 @@ package view.login_page;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("page_login.fxml"));
         primaryStage.setTitle("PNR - Login");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setScene(new Scene(root, screenBounds.getWidth() * 0.999, screenBounds.getHeight() * 0.999));
         primaryStage.show();
     }
 
