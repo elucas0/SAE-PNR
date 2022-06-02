6f1c86326b63bd4ccc4a0a792e0bee045d7b88e9
Elouann Lucas <elouannlucas56@gmail.com>
2022-06-02 12:26:41 +0200

    Amogus

--- src/view/hellofx/Main.java
+++ src/view/hellofx/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Amogus");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
