625af843650c2a63da7ab88093212b5c4a618cdd
Timothee <timothee.posselt@gmail.com>
2022-06-03 08:50:55 +0200

    debut requete

---
+++ src/RequeteLieu.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.sql.*;
+public class RequeteLieu {
+
+
+
+    public static void main(String[] args) {
+        try {
+            //Les ArrayList = au colonne de la table
+            ArrayList<String> coordLambertX = new ArrayList<String>();
+            ArrayList<String> coordLambertY = new ArrayList<String>();
+            
+
+            //Création de la requête SQL
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/pnr", "base_donnee", "sC32DnE3ae7Y");
+            Statement s = c.createStatement();
+            String query = "SELECT * FROM Lieu";
+            ResultSet r = s.executeQuery(query);
+
+            //Remplissage des ArrayList
+            while (r.next()) {
+                coordLambertX.add(r.getString("coord_Lambert_X"));
+                coordLambertY.add(r.getString("coord_Lambert_Y"));
+                
+            }
+            //Suppression des virgules
+            coordLambertX.toString().replaceAll(",", " ");
+            coordLambertY.toString().replaceAll(",", " ");
+
+            //Affichage de la ligne 1
+            System.out.println(coordLambertX.get(0));
+            System.out.println(coordLambertY.get(0));
+
+            r.close();
+            s.close();
+            c.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }    
+}
