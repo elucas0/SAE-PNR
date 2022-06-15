f27b726f4df05a41b48cb21a6cff9be78958947f
Lemap1 <maelphilippe25@gmail.com>
2022-06-15 11:54:10 +0200

    fin fxml Maël

--- src/controller/Formulaire_nid_gci_controller.java
+++ src/controller/Formulaire_nid_gci_controller.java
@@ -1,37 +1,32 @@
 package controller;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.text.Font;
-import org.kordamp.ikonli.javafx.FontIcon;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Formulaire_nid_gci_controller {
 
     @FXML
-    private ComboBox natureObs;
-    
-    public void setItems(){
+    private ComboBox<String> natureObs;
 
-        ArrayList<String> contenu = new ArrayList<String>();
-        contenu.add("Poussin");
-        contenu.add("Oeuf");
-        contenu.add("Nid");
-        natureObs.getItems().addAll();
+    private ObservableList<String> liste;
+
+    public Formulaire_nid_gci_controller(){
+
+
+    }
+    
+
+
+    @FXML
+    private void initialize() 
+    {
+        liste = FXCollections.observableArrayList("test", "test");
+
+        natureObs.setItems(liste);
     }
     
 }
