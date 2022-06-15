8120117fc5c7310adeea9896d3a2d439b7671d1d
Lemap1 <maelphilippe25@gmail.com>
2022-06-15 10:45:12 +0200

    lo

--- src/view/login_page/Main.java
+++ src/view/login_page/Main.java
@@ -1,4 +1,4 @@
-package view;
+package view.login_page;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Formulaire_nid_gci.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Formulaire_nid_gci.fxml"));
         primaryStage.setTitle("PNR - Login");
         primaryStage.setScene(new Scene(root, screenBounds.getWidth(), screenBounds.getHeight() * 0.97));
         primaryStage.show();
