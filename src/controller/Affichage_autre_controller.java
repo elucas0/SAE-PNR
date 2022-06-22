814164131278d4fd89f5e8a35f839cc55fb55605
Lemap1 <maelphilippe25@gmail.com>
2022-06-22 14:40:50 +0200

    affichage lieux et observations

---
+++ src/controller/Affichage_autre_controller.java
@@ -0,0 +1,116 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+public class Affichage_autre_controller {
+
+    @FXML
+    /**
+     * The retour button in the fxml file
+     */
+    private Button retour;
+
+    @FXML
+    private TableView<String> table;
+
+    private TableColumn<?, ?> colonne;
+
+    @FXML
+    private Button user;
+    
+
+
+    @FXML
+    /**
+     * Initialize elements when the fxml file is dilpayed
+     */
+    private void initialize() 
+    {
+
+
+    }
+
+
+    /**
+    * Event to do when the button retour is pressed.    
+    * Switch to the page choix_stat_liste.fxml
+    */
+    public void retour(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        change.go_to("../view/Affichage.fxml");
+    }
+
+
+    /**
+    * Event to do when the button retour is pressed.    
+    * Switch to the page Accueil_Utilisateur.fxml
+    */
+    public void home(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        if(ReadInfos.estAdmin()){
+
+            change.go_to("../view/Accueil_Admin.fxml");
+        }else{
+
+            change.go_to("../view/Accueil_Utilisateur.fxml");
+        }
+    }
+
+
+    public void affichage_observateur(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        change.go_to("../view/Affichage_observateur.fxml");
+
+    }
+
+    public void affichage_lieu(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        change.go_to("../view/Affichage_lieu.fxml");
+
+    }
+
+    public void affichage_batracien(){}
+
+
+    public void affichage_loutre(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        //change.go_to("../view/Affichage_loutre.fxml");       
+    }
+
+
+    public void affichage_gci(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        //change.go_to("../view/Affichage_loutre.fxml");       
+    }
+
+    public void affichage_hippocampe(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        //change.go_to("../view/Affichage_loutre.fxml");       
+    }
+
+
+    public void affichage_chouette(){
+
+        Stage actuel = (Stage)retour.getScene().getWindow();
+        ChangerPage change = new ChangerPage(actuel);
+        //change.go_to("../view/Affichage_loutre.fxml");       
+    }
+    
+}
