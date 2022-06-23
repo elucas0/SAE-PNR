b1f6139af3cead73f0380e9882cc05d93ce51f7c
Timothee <timothee.posselt@gmail.com>
2022-06-23 11:26:29 +0200

    obs

--- src/controller/formulaires/Observateur_controller.java
+++ src/controller/formulaires/Observateur_controller.java
@@ -101,6 +101,7 @@ public class Observateur_controller {
                     admin = 0;
                 }
 
+
                 PreparedStatement testRegistration = c.prepareStatement("SELECT * FROM registration WHERE id = ? AND full_name = ? AND password = ? AND administration = ?");
                 testRegistration.setString(1, idCompte.getText());
                 testRegistration.setString(2, prenom.getText());
@@ -108,20 +109,15 @@ public class Observateur_controller {
                 testRegistration.setInt(4, admin);
                 ResultSet resultatRegistration = testRegistration.executeQuery();
 
-                PreparedStatement id_Registration = c.prepareStatement("SELECT MAX(id) FROM registration;");
-                ResultSet requete2 = id_Registration.executeQuery();
-                requete2.next();
-                int idR = requete2.getInt("Max(id)");
-
                 if(resultatRegistration.next()){
                     showAlert(Alert.AlertType.ERROR, owner, "Registration", "Compte déjà créée!");
                 }
                 else{
-                    String querry1 = "INSERT INTO registration VALUES(" + idR + ",'" + prenom.getText() + "','" + mdp.getText() + "','" + admin + "');";
+                    String querry1 = "INSERT INTO registration VALUES(" + idCompte.getText() + ",'" + prenom.getText() + "','" + mdp.getText() + "','" + admin + "');";
                     ObservateurController.executeUpdate(querry1);
                     showAlert(Alert.AlertType.CONFIRMATION, owner, "Observateur", "rentré!");
                 }
-
+                
                 PreparedStatement testObservateur = c.prepareStatement("SELECT * FROM Observateur WHERE nom = ? AND prenom = ?");
                 testObservateur.setString(1, nom.getText());
                 testObservateur.setString(2, prenom.getText());
@@ -136,8 +132,6 @@ public class Observateur_controller {
                     showAlert(Alert.AlertType.CONFIRMATION, owner, "Observateur", "rentré!");
 
                 }
-    
-                
             } catch (Exception e) {
                 e.printStackTrace();
             }
